b911da557a12d077f5f8f02ef2389360e19f379b
kwonja <kownja87@gmail.com>
2023-06-16 07:10:23 +0900

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {NavLink, Link,useNavigate } from "react-router-dom";
 import '../App.css';
 import styled from 'styled-components';
